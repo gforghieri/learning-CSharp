7f6a73b33aa102e8b6d5a5939b089f4567898a04
Giuliano Forghieri <gfo@netcompany.com>
2020-07-06 16:03:19 +0200

    add WorkWithDoubles

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace NumbersInCSharp
     {
         public static void Main(string[] args)
         {
-            TestLimits();
+            WorkWithDoubles();
+        }
+
+
+        public static void WorkWithDoubles()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e + f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
         }
 
 
