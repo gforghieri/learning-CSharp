906552a7793963b8f622124d930b0864c0ab9624
Giuliano Forghieri <gfo@netcompany.com>
2020-07-06 14:04:23 +0200

    update gitignore and add integer math

--- Program.cs
+++ Program.cs
@@ -4,9 +4,30 @@ namespace NumbersInCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => WorkingWithIntegers();
+
+
+        static void WorkingWithIntegers()
         {
-            Console.WriteLine("Hello World!");
+            int a = 18;
+            int b = 6;
+
+
+            // addition
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
         }
     }
 }
