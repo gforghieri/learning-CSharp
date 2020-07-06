f5431f9d17e05975d619f9f2ce242cce5edccd8a
Giuliano Forghieri <gfo@netcompany.com>
2020-07-06 16:11:13 +0200

    add WorkWithDecimals

--- Program.cs
+++ Program.cs
@@ -6,9 +6,23 @@ namespace NumbersInCSharp
     {
         public static void Main(string[] args)
         {
-            WorkWithDoubles();
+            WorkWithDecimals();
         }
 
+        public static void WorkWithDecimals()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+        }
 
         public static void WorkWithDoubles()
         {
