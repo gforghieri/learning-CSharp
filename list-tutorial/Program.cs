6f3bd6e9c64ba3163caaa06bbbb3b512db89456f
Giuliano Forghieri <gfo@netcompany.com>
2020-07-07 14:01:18 +0200

    add lists with strings

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -7,6 +7,12 @@ namespace list_tutorial
     {
         static void Main(string[] args)
         {
+            WorkingWithString();
+        }
+
+
+        static void WorkingWithString()
+        {
             var names = new List<string> { "Giuliano", "Ana", "Felipe" };
             foreach (var name in names)
             {
@@ -26,6 +32,38 @@ namespace list_tutorial
             Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
 
             Console.WriteLine($"The list has {names.Count} people in it");
+
+
+            var index = names.IndexOf("Felipe");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+
         }
+
+
     }
 }
