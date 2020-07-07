35af54f31180ef961e3eb61c6c4c2fde18b85fd0
Giuliano Forghieri <gfo@netcompany.com>
2020-07-07 10:13:18 +0200

    add basic loops

--- BranchesAndLoops/BranchesAndLoopsProgram.cs
+++ BranchesAndLoops/BranchesAndLoopsProgram.cs
@@ -43,7 +43,45 @@ namespace BranchesAndLoops
 
         static void Main(string[] args)
         {
-            ExploreIf();
+            // int counter = 0;
+            // while (counter < 10)
+            // {
+            //     Console.WriteLine($"Hello World! The counter is {counter}");
+            //     counter++;
+            // }
+
+            // int counter = 0;
+            // do
+            // {
+            //     Console.WriteLine($"Hello World! The counter is {counter}");
+            //     counter++;
+            // } while (counter < 10);
+            //
+            // for (int index = 0; index < 10; index++)
+            // {
+            //     Console.WriteLine($"Hello World! The index is {index}");
+            // }
+
+
+            for (int row = 1; row < 11; row++)
+            {
+                Console.WriteLine($"The row is {row}");
+            }
+
+            for (char column = 'a'; column < 'k'; column++)
+            {
+                Console.WriteLine($"The column is {column}");
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+
+
         }
     }
 }
