6050415770178f7a3f13ba7132b720127774bb0b
Giuliano Forghieri <gfo@netcompany.com>
2020-07-08 11:44:17 +0200

    add fibonacci list challenge

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace list_tutorial
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            WorkingWithString();
+            var fibonacciNumbers = new List<int> {1, 1};
+
+            while (fibonacciNumbers.Count < 20)
+            {
+                int previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                int previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                fibonacciNumbers.Add(previous + previous2);
+            }
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
 
 
