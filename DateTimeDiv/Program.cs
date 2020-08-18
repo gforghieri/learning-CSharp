364d397687470ad259916466ccc379e24d936527
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-08-18 17:14:03 +0200

    add DateTime exercise

---
+++ DateTimeDiv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DateTimeDiv
+{
+    class Program
+    {
+
+        private static DateTime endDate = new DateTime(2005, 12, 22);
+        private static DateTime startDate = new DateTime(2005, 5, 2);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTimeNeg(endDate,startDate));
+        }
+
+        static  TimeSpan DateTimeNeg(DateTime dateTime1, DateTime dateTime2)
+        {
+            return dateTime1 - dateTime2;
+        }
+
+
+        static void DateTimeDiv(TimeSpan timeSpan1, TimeSpan timespan2)
+        {
+
+        }
+
+    }
+}
